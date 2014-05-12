61b3840c83ca899d860635bb59c8ed474464f7a8
erelea00 <erelea00@estudiantes.unileon.es>
2014-05-12 23:08:20 +0200

    Added test cases for the front controller

---
+++ src/test/java/es/unileon/administrationFeeViewer/web/AdministrationFeeViewerControllerTests.java
@@ -0,0 +1,17 @@
+package es.unileon.administrationFeeViewer.web;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+public class AdministrationFeeViewerControllerTests {
+
+	@Test
+	public void testHandleRequestView() throws Exception{		
+        AdministrationFeeViewerController controller = new AdministrationFeeViewerController();
+        ModelAndView modelAndView = controller.handleRequest(null, null);		
+        assertEquals("mainPage.jsp", modelAndView.getViewName());
+    }
+
+}
